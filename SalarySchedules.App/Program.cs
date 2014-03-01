f2fb708c384fff745f9fafe0334e528925b46aa7
Kegan Maher <kegan.maher@smgov.net>
2014-03-01 14:21:24 -0800

    move itext ref to Parser, give App references

--- SalarySchedules.App/Program.cs
+++ SalarySchedules.App/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SalarySchedules;
 
 namespace SalarySchedules.App
 {
@@ -9,6 +12,43 @@ namespace SalarySchedules.App
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1)
+            {
+                return;
+            }
+
+            List<SalarySchedule> shedules = new List<SalarySchedule>();
+
+            foreach (var file in args)
+            {
+                
+            }
+        }
+
+        static DateTime readReportDate(string file)
+        {
+            DateTime reportDate = DateTime.MinValue;
+
+            using (var reader = new PdfReader(file))
+            {
+                var pageContent 
+            }
+        }
+
+        static IEnumerable<string> readDataToText(string file)
+        {
+            List<string> content = new List<string>();
+
+            using (var reader = new PdfReader(file))
+            {
+                for (int i = 2; i <= reader.NumberOfPages; i++)
+                {
+                    var pageContent = PdfTextExtractor.GetTextFromPage(reader, i, new LocationTextExtractionStrategy());
+                    content.Add(pageContent);
+                }
+            }
+
+            return content;
         }
     }
 }
