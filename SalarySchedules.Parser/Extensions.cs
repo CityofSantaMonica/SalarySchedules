da1c0ebd141f766dc68dbbf98921ff68adc095de
Kegan Maher <kegan.maher@smgov.net>
2014-03-01 19:47:23 -0800

    shortcut to read text from a page

---
+++ SalarySchedules.Parser/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace SalarySchedules.Parser
+{
+    public static class Extensions
+    {
+        public static string TextFromPage(this PdfReader reader, int pageNumber)
+        {
+            var strategy = new LocationTextExtractionStrategy();
+            return PdfTextExtractor.GetTextFromPage(reader, pageNumber, strategy);
+        }
+    }
+}
