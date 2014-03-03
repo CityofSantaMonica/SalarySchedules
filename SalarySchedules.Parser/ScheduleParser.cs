39551c6a7e8606943505b672fe2ad9f45ee19e29
Kegan Maher <kegan.maher@smgov.net>
2014-03-02 19:38:49 -0800

    remove unused usings

--- SalarySchedules.Parser/ScheduleParser.cs
+++ SalarySchedules.Parser/ScheduleParser.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 using iTextSharp.text.pdf;
-using iTextSharp.text.pdf.parser;
 using SalarySchedules.Models;
 
 namespace SalarySchedules.Parser
