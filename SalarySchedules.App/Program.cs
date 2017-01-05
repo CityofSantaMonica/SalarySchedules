ae5e2eb0ec8e4e13ae20e12cb69c7016ff1faecb
Kegan Maher <kegan.maher@smgov.net>
2017-01-05 12:48:53 -0800

    usage notes for CLI

--- SalarySchedules.App/Program.cs
+++ SalarySchedules.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json;
 using SalarySchedules.Models;
 using SalarySchedules.Parser;
@@ -10,24 +11,31 @@ namespace SalarySchedules.App
     {
         static void Main(string[] args)
         {
-            if (args.Length < 1)
+            var files = args.Where(a => a.EndsWith(".pdf", StringComparison.OrdinalIgnoreCase));
+
+            if (!files.Any())
             {
+                Console.WriteLine("USAGE:");
+                Console.WriteLine("\tSalarySchedules file1.pdf [file2.pdf file3.pdf fileN.pdf]");
                 return;
             }
 
             ISalaryScheduleParser parser = new CSMSalaryScheduleParser();
 
-            foreach (var file in args)
+            foreach (var file in files)
             {
                 try
                 {
+                    Console.Write("Processing file {0}... ", file);
                     ISalarySchedule schedule = parser.Process(file);
                     string json = JsonConvert.SerializeObject(schedule);
                     File.WriteAllText(file.Replace(".pdf", ".json"), json);
+                    Console.WriteLine("Finished");
                 }
-                catch
+                catch (Exception ex)
                 {
                     Console.WriteLine("Error processing file {0}", file);
+                    Console.Write(ex);
                 }
             }
         }
